d8098d0c096a08c1687af23aba3b3de1bff93921
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 18:07:33 +0530

    Create TransferOwnership.js

---
+++ frontend/src/pages/TransferOwnership.js
@@ -0,0 +1,15 @@
+function TransferOwnership() {
+
+    return (
+        <div>
+            <h2>Transfer Ownership</h2>
+
+            <p>
+                This page enables authorized users to transfer product ownership from one supply chain participant to another while recording the transaction on the blockchain.
+            </p>
+        </div>
+    );
+
+}
+
+export default TransferOwnership;
