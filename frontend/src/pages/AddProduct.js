9b24e16a1b9f2810cbd75764f5efe7cf33d19651
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 18:01:35 +0530

    Create AddProduct.js

---
+++ frontend/src/pages/AddProduct.js
@@ -0,0 +1,15 @@
+function AddProduct() {
+
+    return (
+        <div>
+            <h2>Add Product</h2>
+
+            <p>
+                This page will allow farmers and administrators to register new agricultural products on the blockchain.
+            </p>
+        </div>
+    );
+
+}
+
+export default AddProduct;
