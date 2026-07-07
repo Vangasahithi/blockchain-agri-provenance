839194bfca02ddb7daefdeaca3a8acaca7baaee3
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:49:25 +0530

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,30 +1,59 @@
 exports.getProducts = async (req, res) => {
+
     res.json({
         status: "success",
-        message: "All Products",
-        data: []
+        data: [
+            {
+                id: "P001",
+                name: "Rice",
+                farmer: "Ramesh",
+                location: "Hyderabad",
+                status: "Harvested"
+            },
+            {
+                id: "P002",
+                name: "Tomatoes",
+                farmer: "Suresh",
+                location: "Warangal",
+                status: "In Transit"
+            },
+            {
+                id: "P003",
+                name: "Mangoes",
+                farmer: "Anil",
+                location: "Nagarkurnool",
+                status: "Delivered"
+            }
+        ]
     });
+
 };
 
 exports.getProduct = async (req, res) => {
+
     res.json({
         status: "success",
         productId: req.params.id
     });
+
 };
 
 exports.createProduct = async (req, res) => {
+
     res.json({
         status: "success",
         message: "Product Created",
         product: req.body
     });
+
 };
 
 exports.transferOwnership = async (req, res) => {
+
     res.json({
         status: "success",
         productId: req.params.id,
         message: "Ownership Transferred"
     });
+
 };
