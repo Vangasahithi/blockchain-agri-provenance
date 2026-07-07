663bcad3e90df88327a60fdcf70ca36025cc15b1
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 18:05:14 +0530

    Create ProductDetails.js

---
+++ frontend/src/pages/ProductDetails.js
@@ -0,0 +1,15 @@
+function ProductDetails() {
+
+    return (
+        <div>
+            <h2>Product Details</h2>
+
+            <p>
+                This page displays detailed provenance information of a selected product, including farmer details, location, harvest date, and blockchain transaction history.
+            </p>
+        </div>
+    );
+
+}
+
+export default ProductDetails;
