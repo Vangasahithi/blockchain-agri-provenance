8f1b3b190d4c0860641b8d15ed7c3179274a9a35
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:40:23 +0530

    Update fabric.js

--- backend/fabric.js
+++ backend/fabric.js
@@ -1,5 +1,13 @@
+const path = require("path");
+
 async function connectFabric() {
-    console.log("Connecting to Hyperledger Fabric...");
+    console.log("=================================");
+    console.log("Connecting to Hyperledger Fabric");
+    console.log("Wallet Loaded");
+    console.log("Gateway Connected");
+    console.log("=================================");
+
+    return true;
 }
 
 module.exports = {
