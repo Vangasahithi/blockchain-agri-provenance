59a987275c72a95ed31362089ef4860d9ac8673d
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:54:14 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav
+            style={{
+                background: "#2E7D32",
+                color: "white",
+                padding: "15px",
+                marginBottom: "20px"
+            }}
+        >
+            <h2>Blockchain Agri Provenance System</h2>
+        </nav>
+    );
+}
+
+export default Navbar;
