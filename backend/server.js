dbcd61326b50673a8e7811c5fa7a144e482ea927
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:21:52 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Blockchain Agri Provenance Backend Running"
+    });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
