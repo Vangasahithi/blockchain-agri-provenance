244266dd7782795cb3dfbc65dd59bbe05249adfb
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:58:13 +0530

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+
+    return (
+        <div>
+            <h2>Welcome to Blockchain Agri Provenance</h2>
+
+            <p>
+                This dashboard helps track agricultural products from farmers to consumers using Hyperledger Fabric blockchain.
+            </p>
+        </div>
+    );
+
+}
+
+export default Home;
