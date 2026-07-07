60e10976b09039566fbe5b4089172941acf247bc
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:30:44 +0530

    product.go

--- chaincode/product.go
+++ chaincode/product.go
@@ -1,1 +1,10 @@
+package main
 
+type Product struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Farmer      string `json:"farmer"`
+	Location    string `json:"location"`
+	HarvestDate string `json:"harvestDate"`
+	Status      string `json:"status"`
+}
