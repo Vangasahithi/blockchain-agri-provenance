7bb2a12b37cc5c38572801dcdf44d8d30d9d8a22
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:43:41 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const {
-    getProducts
+    getProducts,
+    getProduct,
+    createProduct,
+    transferOwnership
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.get("/", getProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id/transfer", transferOwnership);
 
 module.exports = router;
