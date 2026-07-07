a1c60c5642727fc669f88c47611c65aa6d17b5fb
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:35:14 +0530

    smartcontract.go

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/hyperledger/fabric-contract-api-go/contractapi"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
 
 type SmartContract struct {
 	contractapi.Contract
+}
+
+// CreateProduct creates a new product on the ledger
+func (s *SmartContract) CreateProduct(
+	ctx contractapi.TransactionContextInterface,
+	id string,
+	name string,
+	farmer string,
+	location string,
+	harvestDate string,
+	status string,
+) error {
+
+	product := Product{
+		ID:          id,
+		Name:        name,
+		Farmer:      farmer,
+		Location:    location,
+		HarvestDate: harvestDate,
+		Status:      status,
+	}
+
+	productJSON, err := json.Marshal(product)
+
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, productJSON)
 }
