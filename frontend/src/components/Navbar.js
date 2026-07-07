4e8e4929fc14c43ae40687c4275f3a10dd3d87a4
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 19:58:00 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,28 @@
 function Navbar() {
+
     return (
         <nav
             style={{
-                background: "#2E7D32",
+                backgroundColor: "#1B5E20",
                 color: "white",
-                padding: "15px",
-                marginBottom: "20px"
+                padding: "18px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                borderRadius: "8px"
             }}
         >
-            <h2>Blockchain Agri Provenance System</h2>
+            <h2>🌾 Blockchain Agri Provenance</h2>
+
+            <div>
+                <span style={{ marginRight: "20px" }}>Dashboard</span>
+                <span style={{ marginRight: "20px" }}>Products</span>
+                <span style={{ marginRight: "20px" }}>Blockchain</span>
+                <span>Supply Chain</span>
+            </div>
         </nav>
     );
+
 }
 
 export default Navbar;
