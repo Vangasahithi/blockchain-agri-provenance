91fac2bd97a7316d1028bd017930d48ffb58a9a5
Vangasahithi <126494488+Vangasahithi@users.noreply.github.com>
2026-07-07 17:20:22 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+function App() {
+    return (
+        <div>
+            <h1>Blockchain-Based Provenance Tracking</h1>
 
+            <p>
+                Agricultural Supply Chain Management System
+            </p>
+        </div>
+    );
+}
+
+export default App;
